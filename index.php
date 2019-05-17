879713165a32eee6fd71d501d0293af3d6cedaeb
popobravo123 <bravoarocardova@gmail.com>
2019-05-17 12:02:11 +0700

    Update and rename index.html to index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Testing</title>
+</head>
+<body>
+  <table>
+    <?php for ($i=0; $i<5; $i++): ?>
+    <tr>
+      <td><?= $i ?></td>
+    </tr>
+    <?php endfor ?>
+  </table>
+</body>
+</html>
